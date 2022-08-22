135eede4d5e54702b51025e9292b11d5f9b9d22d
ArturAvv <avvakumovart@gmail.com>
2022-08-22 13:58:37 +0300

    smallestNumber

--- self.js
+++ self.js
@@ -1,11 +1,22 @@
 // find smallest number
-const numbers = [2, 5, 35, 56, 12, 24, 7, 80, 3];
-function findSmallerNumber() {
-  let smallNumber = numbers[0];
-  for (let number of numbers) {
-    if (smallNumber < number) {
-      return smallNumber;
+
+const numbers = [15, 2, 5, 35, 56, 1, 12, 24, -7, 80, 3];
+function findSmallerNumber(numbers) {
+  if (Array.isArray(numbers)) {
+    let smallNumber = numbers[0];
+
+    for (let number of numbers) {
+      if (smallNumber > number) {
+        smallNumber = number;
+      }
     }
+
+    return smallNumber;
   }
+  return `Not an array`;
 }
 console.log(findSmallerNumber(numbers));
+
+// write function that returns array with uniq elements
+
+const words = ["HTML", "CSS", "JS", "CSS", "JS"]
